0176f1e44c33defc73fa121bebb8f5a549d8d3db
Vladislav <volf10011@gmail.com>
2020-04-06 19:59:35 +0300

    fix: fix unrunnable code

--- drop/main.sql
+++ drop/main.sql
@@ -6,10 +6,8 @@ DEFINE USER_NAME = &&1
 PROMPT 'run drop_schema with param user_name = &&USER_NAME'
 @drop_schema.sql &&USER_NAME
 
-
 PROMPT 'run drop_tablespace.sql with param user_name = &&USER_NAME'
 @drop_tablespace.sql &&USER_NAME
-
 
 UNDEFINE USER_NAME
 SPOOL OFF
--- install/main.sql
+++ install/main.sql
@@ -4,17 +4,21 @@ SPOOL INSTALL_FILE.LOG
 DEFINE USER_NAME = &&1
 DEFINE TBS_LOCATION = &&2
 
-PROMPT  execute create_tablespace.sql for data tablespace
-        user_name = &&USER_NAME
-        file location = &&TBS_LOCATION
+PROMPT  "execute create_tablespace.sql for data tablespace"
+PROMPT  "user_name = &&USER_NAME"
+PROMPT  "file location = &&TBS_LOCATION"
 
 @create_tablespace.sql &&USER_NAME &&TBS_LOCATION DATA
 
-PROMPT  execute create_tablespace.sql for index tablespace
-        user_name = &&USER_NAME
-        file location = &&TBS_LOCATION
+PROMPT  "execute create_tablespace.sql for index tablespace"
+PROMPT  "user_name = &&USER_NAME"
+PROMPT  "file location = &&TBS_LOCATION"
 
 @create_tablespace.sql &&USER_NAME &&TBS_LOCATION IDX
+
+@create_schema.sql &&USER_NAME
+
+@privilege_schema.sql &&USER_NAME
 
 
 UNDEFINE USER_NAME
